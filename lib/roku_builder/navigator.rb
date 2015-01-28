f404db796b8bfaf6defa6ab7fe5128edacd2aab1
Charles Greene <charles.greene@redspace.com>
2015-01-28 16:31:42 -0400

    add navigator with navigate, restart, and type commands

---
+++ lib/roku_builder/navigator.rb
@@ -0,0 +1,47 @@
+module RokuBuilder
+  class Navigator < Util
+    def init
+      @commands = {
+        up: "Up",
+        down: "Down",
+        right: "Right",
+        left: "Left",
+        select: "Select",
+        back: "Back",
+        home: "Home",
+        rew: "Rev",
+        ff: "Fwd",
+        play: "Play",
+        replay: "InstantReplay"
+      }
+    end
+    def nav(command:)
+      if @commands.has_key?(command)
+        conn = Faraday.new(url: "#{@url}:8060") do |f|
+          f.request :multipart
+          f.request :url_encoded
+          f.adapter Faraday.default_adapter
+        end
+        payload =  {}
+
+        path = "/keypress/#{@commands[command]}"
+        response = conn.post path, payload
+      else
+        return false
+      end
+    end
+    def type(text:)
+      conn = Faraday.new(url: "#{@url}:8060") do |f|
+        f.request :multipart
+        f.request :url_encoded
+        f.adapter Faraday.default_adapter
+      end
+      payload =  {}
+      text.split(//).each do |c|
+        path = "/keypress/LIT_#{CGI::escape(c)}"
+        puts "#{@url}:8060#{path}"
+        response = conn.post path, payload
+      end
+    end
+  end
+end
--- lib/roku_builder/util.rb
+++ lib/roku_builder/util.rb
@@ -11,6 +11,11 @@ module RokuBuilder
       @dev_username = user
       @dev_password = password
       @url = "http://#{@roku_ip_address}"
+      init()
+    end
+
+    def init
+      #Override in subclass
     end
   end
 end
