e7efee0d50c9d05b29e376fcdbc934b90cd1ffe9
Charles Greene <charles.greene@redspace.com>
2016-02-05 10:12:01 -0400

    add inspector and keyer tests

--- tests/inspector_test.rb
+++ tests/inspector_test.rb
@@ -1,6 +1,63 @@
 require "roku_builder"
 require "minitest/autorun"
+require 'byebug'
 
 class InspectorTest < Minitest::Test
-  #TODO
+  def test_inspector_inspect
+    connection = Minitest::Mock.new
+    faraday = Minitest::Mock.new
+    io = Minitest::Mock.new
+    response = Minitest::Mock.new
+
+    device_config = {
+      ip: "111.222.333",
+      user: "user",
+      password: "password"
+    }
+    path = "/plugin_inspect"
+    password = "password"
+    payload ={
+      mysubmit: "Inspect",
+      password: password,
+      archive: io
+    }
+    body = "<td>App Name:</td><td><font>app_name</font></td>"+
+           "<td>Dev ID:</td><td><font>dev_id</font></td>"+
+           "<td>Creation Date:</td><td><font><script>var d = new Date(628232400)</script></font></td>"+
+           "<td>dev.zip:</td><td><font>dev_zip</font></td>"
+
+    connection.expect(:post, response) do |arg1, arg2|
+      assert_equal path, arg1
+      assert_equal payload[:mysubmit], arg2[:mysubmit]
+      assert_equal payload[:password], arg2[:passwd]
+      assert payload[:archive] === arg2[:archive]
+    end
+    faraday.expect(:request, nil, [:digest, device_config[:user], device_config[:password]])
+    faraday.expect(:request, nil, [:multipart])
+    faraday.expect(:request, nil, [:url_encoded])
+    faraday.expect(:adapter, nil, [Faraday.default_adapter])
+    response.expect(:body, body)
+    response.expect(:body, body)
+    response.expect(:body, body)
+    response.expect(:body, body)
+
+
+    package_info = {}
+    inspector = RokuBuilder::Inspector.new(**device_config)
+    Faraday.stub(:new, connection, faraday) do
+      Faraday::UploadIO.stub(:new, io) do
+        package_info = inspector.inspect(pkg: "pkg/path", password: password)
+      end
+    end
+
+    assert_equal "app_name", package_info[:app_name]
+    assert_equal "dev_id", package_info[:dev_id]
+    assert_equal Time.at(628232400).to_s, package_info[:creation_date]
+    assert_equal "dev_zip", package_info[:dev_zip]
+
+    connection.verify
+    faraday.verify
+    io.verify
+    response.verify
+  end
 end
--- tests/keyer_test.rb
+++ tests/keyer_test.rb
@@ -2,5 +2,85 @@ require "roku_builder"
 require "minitest/autorun"
 
 class KeyerTest < Minitest::Test
-  #TODO
+  def test_keyer_dev_id
+    connection = Minitest::Mock.new
+    faraday = Minitest::Mock.new
+    response = Minitest::Mock.new
+
+    device_config = {
+      ip: "111.222.333",
+      user: "user",
+      password: "password"
+    }
+    path = "/plugin_package"
+    body = "Your Dev ID: <font>dev_id</font>"
+
+    connection.expect(:get, response, [path])
+    faraday.expect(:request, nil, [:digest, device_config[:user], device_config[:password]])
+    faraday.expect(:adapter, nil, [Faraday.default_adapter])
+    response.expect(:body, body)
+
+
+    package_info = {}
+    dev_id = nil
+    keyer = RokuBuilder::Keyer.new(**device_config)
+    Faraday.stub(:new, connection, faraday) do
+      dev_id = keyer.dev_id
+    end
+
+    assert_equal "dev_id", dev_id
+
+    connection.verify
+    faraday.verify
+    response.verify
+  end
+
+  def test_keyer_rekey_changed
+    connection = Minitest::Mock.new
+    faraday = Minitest::Mock.new
+    io = Minitest::Mock.new
+    response = Minitest::Mock.new
+
+    device_config = {
+      ip: "111.222.333",
+      user: "user",
+      password: "password"
+    }
+    path = "/plugin_inspect"
+    password = "password"
+    payload ={
+      mysubmit: "Rekey",
+      password: password,
+      archive: io
+    }
+
+    connection.expect(:post, response) do |arg1, arg2|
+      assert_equal path, arg1
+      assert_equal payload[:mysubmit], arg2[:mysubmit]
+      assert_equal payload[:password], arg2[:passwd]
+      assert payload[:archive] === arg2[:archive]
+    end
+    faraday.expect(:request, nil, [:digest, device_config[:user], device_config[:password]])
+    faraday.expect(:request, nil, [:multipart])
+    faraday.expect(:request, nil, [:url_encoded])
+    faraday.expect(:adapter, nil, [Faraday.default_adapter])
+
+    dev_id = Proc.new { Random.rand(100) }
+    keyer = RokuBuilder::Keyer.new(**device_config)
+    key_changed = nil
+    Faraday.stub(:new, connection, faraday) do
+      Faraday::UploadIO.stub(:new, io) do
+        keyer.stub(:dev_id, dev_id) do
+          key_changed = keyer.rekey(keyed_pkg: "pkg/path", password: password)
+        end
+      end
+    end
+
+    assert key_changed
+
+    connection.verify
+    faraday.verify
+    io.verify
+    response.verify
+  end
 end
