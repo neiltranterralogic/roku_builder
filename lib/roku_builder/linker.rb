28ff1dab55b4783b71fc0fb44f34978b03aaad9e
Charles Greene <charles.greene@redspace.com>
2016-02-12 16:41:22 -0400

    refactor linking logic

--- lib/roku_builder/linker.rb
+++ lib/roku_builder/linker.rb
@@ -1,13 +1,21 @@
 module RokuBuilder
   class Linker < Util
-    def link(options)
+    def link(options:)
       path = "/launch/dev"
-      payload = options
-      if options.has_key?(:content_type) and options.has_key?(:mgid)
-        payload[:entity] = options[:content_type]
-        payload[:mgid] = options[:mgid]
-        payload[:autoplay] = false
+      payload = {}
+      return false unless options
+      opts = options.split(/,\s*/)
+      opts.each do |opt|
+        opt = opt.split(":")
+        key = opt.shift.to_sym
+        value = opt.join(":")
+        payload[key] = value
       end
+
+      unless payload.keys.count > 0
+        return false
+      end
+
       path = "#{path}?#{parameterize(payload)}"
       conn = Faraday.new(url: "#{@url}:8060") do |f|
         f.request :digest, @dev_username, @dev_password
@@ -17,6 +25,7 @@ module RokuBuilder
       end
 
       response = conn.post path
+      return response.success?
     end
 
     private
