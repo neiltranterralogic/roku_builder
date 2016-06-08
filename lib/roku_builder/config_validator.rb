d2658ea68c3e04593a8fa059954fab84e560bf65
Charles Greene <charles.greene@redspace.com>
2016-06-08 14:20:50 -0300

    Mark unused variable

--- lib/roku_builder/config_validator.rb
+++ lib/roku_builder/config_validator.rb
@@ -43,7 +43,7 @@ module RokuBuilder
         config[:projects].each {|project,project_config|
           next if project == :default
           validate_project(codes: codes, project: project_config)
-          project_config[:stages].each {|stage, stage_config|
+          project_config[:stages].each {|_stage, stage_config|
             validate_stage(codes: codes, stage: stage_config, project: project_config)
           }
         }
