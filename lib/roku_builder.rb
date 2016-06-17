cac14da9cb4a4baa29f1ec31a42f3ead21ccc055
Charles Greene <charles.greene@redspace.com>
2016-06-17 13:17:22 -0300

    Move git patches

--- lib/roku_builder.rb
+++ lib/roku_builder.rb
@@ -160,3 +160,30 @@ class ::Hash
     self.merge(second, &merger)
   end
 end
+
+module Git
+  class Stashes
+    def pop(index=nil)
+      @base.lib.stash_pop(index)
+    end
+    def drop(index=nil)
+      @base.lib.stash_drop(index)
+    end
+  end
+  class Lib
+    def stash_pop(id = nil)
+      if id
+        command('stash pop', [id])
+      else
+        command('stash pop')
+      end
+    end
+    def stash_drop(id = nil)
+      if id
+        command('stash drop', [id])
+      else
+        command('stash drop')
+      end
+    end
+  end
+end
