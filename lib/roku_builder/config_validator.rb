2f9c35e8b7d5459ed22e0f4b3811d7026411e033
Charles Greene <charles.greene@redspace.com>
2017-04-05 12:14:22 -0300

    Refactor config validator

--- lib/roku_builder/config_validator.rb
+++ lib/roku_builder/config_validator.rb
@@ -1,68 +1,187 @@
 # ********** Copyright Viacom, Inc. Apache 2.0 **********
 
 module RokuBuilder
-
-  MISSING_DEVICES           = 1
-  MISSING_DEVICES_DEFAULT   = 2
-  DEVICE_DEFAULT_BAD        = 3
-  #                         = 4
-  MISSING_PROJECTS_DEFAULT  = 5
-  PROJECTS_DEFAULT_BAD      = 6
-  DEVICE_MISSING_IP         = 7
-  DEVICE_MISSING_USER       = 8
-  DEVICE_MISSING_PASSWORD   = 9
-  PROJECT_MISSING_APP_NAME  = 10
-  PROJECT_MISSING_DIRECTORY = 11
-  PROJECT_MISSING_FOLDERS   = 12
-  PROJECT_FOLDERS_BAD       = 13
-  PROJECT_MISSING_FILES     = 14
-  PROJECT_FILES_BAD         = 15
-  STAGE_MISSING_BRANCH      = 16
-  STAGE_MISSING_SCRIPT      = 17
-  PROJECT_STAGE_METHOD_BAD  = 18
-  KEY_MISSING_PATH          = 19
-  KEY_MISSING_PASSWORD      = 20
-  INVALID_MAPPING_INFO      = 21
-  MISSING_KEY               = 22
-
-  MISSING_STAGE_METHOD      = -1
-
-  # Validate Config File
   class ConfigValidator
 
-    # Validates the roku config
-    # @param config [Hash] roku config object
-    # @return [Array] error codes for valid config (see self.error_codes)
-    def self.validate_config(config:)
-      codes = []
-      validate_structure(codes: codes, config: config)
-      [:projects, :devices, :keys].each do |type|
-        if config[type]
-          config[type].each do |key, value|
-            next if [:default, :key_dir, :project_dir].include?(key)
-            method = "validate_#{type.to_s[0..-2]}".to_sym
-            attrs = {codes: codes}
-            attrs[type.to_s[0..-2].to_sym] = value
-            send(method, attrs)
-            if value[:stages]
-              value[:stages].each {|_stage, stage_config|
-                validate_stage(codes: codes, stage: stage_config, project: value, config: config)
-              }
-            end
+    VALID_CONFIG              = 0
+    MISSING_DEVICES           = 1
+    MISSING_DEVICES_DEFAULT   = 2
+    DEVICE_DEFAULT_BAD        = 3
+    #                         = 4
+    MISSING_PROJECTS_DEFAULT  = 5
+    PROJECTS_DEFAULT_BAD      = 6
+    DEVICE_MISSING_IP         = 7
+    DEVICE_MISSING_USER       = 8
+    DEVICE_MISSING_PASSWORD   = 9
+    PROJECT_MISSING_APP_NAME  = 10
+    PROJECT_MISSING_DIRECTORY = 11
+    PROJECT_MISSING_FOLDERS   = 12
+    PROJECT_FOLDERS_BAD       = 13
+    PROJECT_MISSING_FILES     = 14
+    PROJECT_FILES_BAD         = 15
+    STAGE_MISSING_BRANCH      = 16
+    STAGE_MISSING_SCRIPT      = 17
+    PROJECT_STAGE_METHOD_BAD  = 18
+    KEY_MISSING_PATH          = 19
+    KEY_MISSING_PASSWORD      = 20
+    INVALID_MAPPING_INFO      = 21
+    MISSING_KEY               = 22
+
+    MISSING_STAGE_METHOD      = -1
+
+    def initialize(config:)
+      @logger = Logger.instance
+      @config = config
+      validate_config
+    end
+
+    def print_errors
+      @codes.each do |code|
+        if code > 0
+          @logger.fatal error_codes[code]
+          if code < 0
+            @logger.warn error_codes[code]
           end
         end
       end
-      if config[:input_mapping]
-        config[:input_mapping].each_value {|info| validate_mapping(codes: codes, mapping: info) }
-      end
-      codes.uniq!
-      codes.push(0) if codes.empty?
-      codes
     end
 
-    # Error code messages for config validation
-    # @return [Array] error code messages
-    def self.error_codes()
+    def is_fatal?
+      !@codes.select{|code| code > 0}.empty?
+    end
+
+    def is_depricated?
+      !@codes.select{|code| code < 0}.empty?
+    end
+
+    def is_valid?
+      @codes.select{|code| code > 0}.empty?
+    end
+
+    private
+
+    def validate_config
+      @codes = []
+      validate_structure
+      [:projects, :devices, :keys, :input_mapping].each do |section|
+        validate_section(section: section) if @config[section]
+      end
+      @codes.uniq!
+      @codes.push(VALID_CONFIG) if @codes.empty?
+    end
+
+    def validate_section(section:)
+      @config[section].each do |key, value|
+        next unless should_validate(key: key)
+        call_validate_method_for_section(section: section, content: value)
+        if has_stages(section_content: value)
+          validate_stages(project_content: value)
+        end
+      end
+    end
+
+    def should_validate(key:)
+      !([:default, :key_dir, :project_dir].include?(key))
+    end
+
+    def call_validate_method_for_section(section:, content:)
+      section_singular = singularize(section: section.to_s)
+      attrs = {}
+      attrs[section_singular] = content
+      method = "validate_#{section_singular}".to_sym
+      send(method, attrs)
+    end
+
+    def singularize(section:)
+      section = section[0..-2] if section.end_with?("s")
+      section.to_sym
+    end
+
+    def has_stages(section_content:)
+      section_content.class == Hash and section_content[:stages]
+    end
+
+    def validate_stages(project_content:)
+      project_content[:stages].each_value {|stage_config|
+        validate_stage(stage: stage_config, project: project_content)
+      }
+    end
+
+
+    def validate_structure
+      errors = [
+        [MISSING_DEVICES, !@config[:devices]],
+        [MISSING_DEVICES_DEFAULT, (@config[:devices] and !@config[:devices][:default])],
+        [DEVICE_DEFAULT_BAD, (@config[:devices] and @config[:devices][:default] and !@config[:devices][:default].is_a?(Symbol))],
+        [MISSING_PROJECTS_DEFAULT, (@config[:projects] and !@config[:projects][:default])],
+        [MISSING_PROJECTS_DEFAULT, (@config[:projects] and @config[:projects][:default] == "<project id>".to_sym)],
+        [PROJECTS_DEFAULT_BAD, (@config[:projects] and @config[:projects][:default] and !@config[:projects][:default].is_a?(Symbol))]
+      ]
+      process_errors(errors: errors)
+    end
+
+    def validate_device(device:)
+      errors = [
+        [DEVICE_MISSING_IP, (!device[:ip])],
+        [DEVICE_MISSING_IP, (device[:ip] == "xxx.xxx.xxx.xxx")],
+        [DEVICE_MISSING_IP, (device[:ip] == "")],
+        [DEVICE_MISSING_USER, (!device[:user])],
+        [DEVICE_MISSING_USER, (device[:user] == "<username>")],
+        [DEVICE_MISSING_USER, (device[:user] == "")],
+        [DEVICE_MISSING_PASSWORD, (!device[:password])],
+        [DEVICE_MISSING_PASSWORD, (device[:password] == "<password>")],
+        [DEVICE_MISSING_PASSWORD, (device[:password] == "")]
+      ]
+      process_errors(errors: errors)
+    end
+
+    def validate_project(project:)
+      errors= [
+        [PROJECT_MISSING_APP_NAME, (!project[:app_name])],
+        [PROJECT_MISSING_DIRECTORY, (!project[:directory])],
+        [PROJECT_MISSING_FOLDERS, (!project[:folders])],
+        [PROJECT_FOLDERS_BAD, (project[:folders] and !project[:folders].is_a?(Array))],
+        [PROJECT_MISSING_FILES, (!project[:files])],
+        [PROJECT_FILES_BAD, (project[:files] and !project[:files].is_a?(Array))],
+        [MISSING_STAGE_METHOD, ( !project[:stage_method])],
+        [PROJECT_STAGE_METHOD_BAD, (![:git, :script, nil].include?(project[:stage_method]))]
+      ]
+      process_errors(errors: errors)
+    end
+
+    def validate_stage(stage:, project:)
+      errors= [
+        [STAGE_MISSING_BRANCH, (!stage[:branch] and project[:stage_method] == :git)],
+        [STAGE_MISSING_SCRIPT, (!stage[:script] and project[:stage_method] == :script)],
+        [MISSING_KEY, (!!stage[:key] and stage[:key].class == String and (!@config[:keys] or !@config[:keys][stage[:key].to_sym]))]
+      ]
+      process_errors(errors: errors)
+    end
+
+    def validate_key(key:)
+      errors= [
+        [KEY_MISSING_PATH, (!key[:keyed_pkg])],
+        [KEY_MISSING_PATH, (key[:keyed_pkg] == "<path/to/signed/package>")],
+        [KEY_MISSING_PASSWORD, (!key[:password])],
+        [KEY_MISSING_PASSWORD, (key[:password] == "<password>")],
+      ]
+      process_errors(errors: errors)
+    end
+
+    def validate_input_mapping(input_mapping:)
+      errors=[
+        [INVALID_MAPPING_INFO, (input_mapping.count != 2)]
+      ]
+      process_errors(errors: errors)
+    end
+
+    def process_errors(errors:)
+      errors.each do |error|
+        @codes.push(error[0]) if error[1]
+      end
+    end
+
+    def error_codes
       [
         #===============FATAL ERRORS===============#
         "Valid Config.",
@@ -91,100 +210,6 @@ module RokuBuilder
         #===============WARNINGS===============#
         "A project is missing its stage method."
       ]
-    end
-
-    # Validates the roku config structure
-    # @param codes [Array] array of error codes
-    # @param config [Hash] roku config object
-    def self.validate_structure(codes:, config:)
-      errors = [
-        [MISSING_DEVICES, !config[:devices]],
-        [MISSING_DEVICES_DEFAULT, (config[:devices] and !config[:devices][:default])],
-        [DEVICE_DEFAULT_BAD, (config[:devices] and config[:devices][:default] and !config[:devices][:default].is_a?(Symbol))],
-        [MISSING_PROJECTS_DEFAULT, (config[:projects] and !config[:projects][:default])],
-        [MISSING_PROJECTS_DEFAULT, (config[:projects] and config[:projects][:default] == "<project id>".to_sym)],
-        [PROJECTS_DEFAULT_BAD, (config[:projects] and config[:projects][:default] and !config[:projects][:default].is_a?(Symbol))]
-      ]
-      process_errors(codes: codes, errors: errors)
-    end
-    private_class_method :validate_structure
-
-    # Validates a roku config device
-    # @param codes [Array] array of error codes
-    # @param device [Hash] device config object
-    def self.validate_device(codes:, device:)
-      errors = [
-        [DEVICE_MISSING_IP, (!device[:ip])],
-        [DEVICE_MISSING_IP, (device[:ip] == "xxx.xxx.xxx.xxx")],
-        [DEVICE_MISSING_IP, (device[:ip] == "")],
-        [DEVICE_MISSING_USER, (!device[:user])],
-        [DEVICE_MISSING_USER, (device[:user] == "<username>")],
-        [DEVICE_MISSING_USER, (device[:user] == "")],
-        [DEVICE_MISSING_PASSWORD, (!device[:password])],
-        [DEVICE_MISSING_PASSWORD, (device[:password] == "<password>")],
-        [DEVICE_MISSING_PASSWORD, (device[:password] == "")]
-      ]
-      process_errors(codes: codes, errors: errors)
-    end
-    private_class_method :validate_device
-
-    # Validates a roku config project
-    # @param codes [Array] array of error codes
-    # @param project [Hash] project config object
-    def self.validate_project(codes:, project:)
-      errors= [
-        [PROJECT_MISSING_APP_NAME, (!project[:app_name])],
-        [PROJECT_MISSING_DIRECTORY, (!project[:directory])],
-        [PROJECT_MISSING_FOLDERS, (!project[:folders])],
-        [PROJECT_FOLDERS_BAD, (project[:folders] and !project[:folders].is_a?(Array))],
-        [PROJECT_MISSING_FILES, (!project[:files])],
-        [PROJECT_FILES_BAD, (project[:files] and !project[:files].is_a?(Array))],
-        [MISSING_STAGE_METHOD, ( !project[:stage_method])],
-        [PROJECT_STAGE_METHOD_BAD, (![:git, :script, nil].include?(project[:stage_method]))]
-      ]
-      process_errors(codes: codes, errors: errors)
-    end
-    private_class_method :validate_project
-
-    # Validates a roku config project
-    # @param codes [Array] array of error codes
-    # @param project [Hash] project config object
-    def self.validate_stage(codes:, stage:, project:, config:)
-      errors= [
-        [STAGE_MISSING_BRANCH, (!stage[:branch] and project[:stage_method] == :git)],
-        [STAGE_MISSING_SCRIPT, (!stage[:script] and project[:stage_method] == :script)],
-        [MISSING_KEY, (!!stage[:key] and stage[:key].class == String and (!config[:keys] or !config[:keys][stage[:key].to_sym]))]
-      ]
-      process_errors(codes: codes, errors: errors)
-    end
-    private_class_method :validate_stage
-
-    # Validates a roku config project
-    # @param codes [Array] array of error codes
-    # @param project [Hash] project config object
-    def self.validate_key(codes:, key:)
-      errors= [
-        [KEY_MISSING_PATH, (!key[:keyed_pkg])],
-        [KEY_MISSING_PATH, (key[:keyed_pkg] == "<path/to/signed/package>")],
-        [KEY_MISSING_PASSWORD, (!key[:password])],
-        [KEY_MISSING_PASSWORD, (key[:password] == "<password>")],
-      ]
-      process_errors(codes: codes, errors: errors)
-    end
-    private_class_method :validate_key
-
-    def self.validate_mapping(codes:, mapping:)
-      errors=[
-        [INVALID_MAPPING_INFO, (mapping.count != 2)]
-      ]
-      process_errors(codes: codes, errors: errors)
-    end
-    private_class_method :validate_mapping
-
-    def self.process_errors(codes:, errors:)
-      errors.each do |error|
-        codes.push(error[0]) if error[1]
-      end
     end
   end
 end
--- test/roku_builder/test_config_validator.rb
+++ test/roku_builder/test_config_validator.rb
@@ -7,190 +7,191 @@ class ConfigValidatorTest < Minitest::Test
   def test_config_manager_validate_devices
     config = good_config
     config[:devices] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [1], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert validator.is_fatal?
+    assert_equal [1], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_devices_default
     config = good_config
     config[:devices][:default] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [2], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [2], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_devices_default_is_symbol
     config = good_config
     config[:devices][:default] = "bad"
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [3], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [3], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_device_ip
     config = good_config
     config[:devices][:roku][:ip] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [7], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [7], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_device_ip_empty
     config = good_config
     config[:devices][:roku][:ip] = ""
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [7], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [7], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_device_ip_default_value
     config = good_config
     config[:devices][:roku][:ip] = "xxx.xxx.xxx.xxx"
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [7], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [7], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_device_user
     config = good_config
     config[:devices][:roku][:user] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [8], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [8], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_device_user_empty
     config = good_config
     config[:devices][:roku][:user] = ""
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [8], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [8], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_device_user_default_value
     config = good_config
     config[:devices][:roku][:user] = "<username>"
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [8], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [8], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_device_password
     config = good_config
     config[:devices][:roku][:password] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [9], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [9], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_device_password_empty
     config = good_config
     config[:devices][:roku][:password] = ""
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [9], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [9], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_device_password_default_value
     config = good_config
     config[:devices][:roku][:password] = "<password>"
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [9], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [9], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_projects
     config = good_config
     config[:projects] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [0], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert validator.is_valid?
   end
 
   def test_config_manager_validate_projects_default
     config = good_config
     config[:projects][:default] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [5], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [5], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_projects_default_default_value
     config = good_config
     config[:projects][:default] = "<project id>".to_sym
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [5], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [5], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_projects_default_is_symbol
     config = good_config
     config[:projects][:default] = "project_id"
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [6], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [6], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_project_app_name
     config = good_config
     config[:projects][:project1][:app_name] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [10], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [10], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_project_directory
     config = good_config
     config[:projects][:project1][:directory] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [11], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [11], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_project_folders
     config = good_config
     config[:projects][:project1][:folders] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [12], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [12], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_project_folders_is_array
     config = good_config
     config[:projects][:project1][:folders] = "Folders"
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [13], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [13], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_project_files
     config = good_config
     config[:projects][:project1][:files] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [14], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [14], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_project_filess_is_array
     config = good_config
     config[:projects][:project1][:files] = "Files"
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [15], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [15], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_project_key
     config = good_config
     config[:projects][:project2][:stages][:production][:key] = "b"
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [22], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [22], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_keys_pkg
     config = good_config
     config[:keys][:a][:keyed_pkg] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [19], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [19], validator.instance_variable_get(:@codes)
     config = good_config
     config[:keys][:a][:keyed_pkg] = "<path/to/signed/package>"
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [19], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [19], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_keys_password
     config = good_config
     config[:keys][:a][:password] = nil
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [20], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [20], validator.instance_variable_get(:@codes)
     config = good_config
     config[:keys][:a][:password] = "<password>"
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [20], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [20], validator.instance_variable_get(:@codes)
   end
 
   def test_config_manager_validate_input_mappings
     config = good_config
     config[:input_mapping]["a"] = ["home"]
-    codes = RokuBuilder::ConfigValidator.validate_config(config: config)
-    assert_equal [21], codes
+    validator = RokuBuilder::ConfigValidator.new(config: config)
+    assert_equal [21], validator.instance_variable_get(:@codes)
   end
 end
