05e5afe7fe0a981577f91d74487a155d85ac16a8
Charles Greene <charles.greene@redspace.com>
2017-05-24 16:44:10 -0300

    Add wemock to linker tests

--- test/roku_builder/test_linker.rb
+++ test/roku_builder/test_linker.rb
@@ -5,78 +5,52 @@ require_relative "test_helper.rb"
 module RokuBuilder
   class LinkerTest < Minitest::Test
     def setup
-      options = build_options
-      @config = Config.new(options: options)
-      @connection = Minitest::Mock.new
-      @faraday = Minitest::Mock.new
-      @response = Minitest::Mock.new
-      @device_config = {
-        ip: "111.222.333",
-        user: "user",
-        password: "password"
-      }
-      @config.instance_variable_set(:@parsed, {device_config: @device_config, init_params: {}})
-
-      @faraday.expect(:headers, {})
-      @faraday.expect(:request, nil, [:digest, @device_config[:user], @device_config[:password]])
-      @faraday.expect(:request, nil, [:multipart])
-      @faraday.expect(:request, nil, [:url_encoded])
-      @faraday.expect(:adapter, nil, [Faraday.default_adapter])
-      @response.expect(:success?, true)
-
+      @config = build_config_object(LinkerTest)
+      @requests = []
     end
     def teardown
-      @connection.verify
-      @faraday.verify
-      @response.verify
+      @requests.each {|req| remove_request_stub(req)}
     end
     def test_linker_link
 
-      path = "/launch/dev?a=A&b=B%3AC&d=a%5Cb"
       options = 'a:A, b:B:C, d:a\b'
 
-      @connection.expect(:post, @response, [path])
+      @requests.push(stub_request(:post, "http://192.168.0.100:8060/launch/dev?a=A&b=B:C&d=a%5Cb").
+        to_return(status: 200, body: "", headers: {}))
 
       linker = Linker.new(config: @config)
-      success = nil
-      Faraday.stub(:new, @connection, @faraday) do
-        success = linker.launch(options: options)
-      end
+      success = linker.launch(options: options)
 
       assert success
     end
     def test_linker_link_nothing
-      path = "/launch/dev"
+
+      @requests.push(stub_request(:post, "http://192.168.0.100:8060/launch/dev").
+        to_return(status: 200, body: "", headers: {}))
       options = ''
-      @connection.expect(:post, @response, [path])
       linker = Linker.new(config: @config)
-      success = nil
-      Faraday.stub(:new, @connection, @faraday) do
-        success = linker.launch(options: options)
-      end
+      success = linker.launch(options: options)
 
       assert success
     end
 
     def test_linker_list
-      path = "/query/apps"
       body = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>\n<apps>\n\t
       <app id=\"31012\" type=\"menu\" version=\"1.6.3\">Movie Store and TV Store</app>\n\t
       <app id=\"31863\" type=\"menu\" version=\"1.2.6\">Roku Home News</app>\n\t
       <app id=\"65066\" type=\"appl\" version=\"1.3.0\">Nick</app>\n\t
       <app id=\"68161\" type=\"appl\" version=\"1.3.0\">Nick</app>\n\t
       </apps>\n"
-      @connection.expect(:get, @response, [path])
-      @response.expect(:body, body)
+      @requests.push(stub_request(:get, "http://192.168.0.100:8060/query/apps").
+        to_return(status: 200, body: body, headers: {}))
+
       linker = Linker.new(config: @config)
 
       print_count = 0
       did_print = Proc.new { |msg| print_count+=1 }
 
-      Faraday.stub(:new, @connection, @faraday) do
-        linker.stub(:printf, did_print) do
-          linker.list
-        end
+      linker.stub(:printf, did_print) do
+        linker.list
       end
 
       assert_equal 6, print_count
